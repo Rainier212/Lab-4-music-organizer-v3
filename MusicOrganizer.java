c2de14a961c2e21b8d1971bf7eb5d429ebe46683
Rainier <rainiertabing21@gmail.com>
2025-09-26 21:19:43 -0400

    Added a listMatching method

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -140,4 +140,18 @@ public class MusicOrganizer
         }
         return valid;
     }
+    
+    /**
+     * List the names of files matching the given search string.
+     * @param searchString The string to match.
+     */
+    public void listMatching(String searchString)
+    {
+        for(String filename : files){
+            if (filename.contains(searchString)){
+                // A match.
+                System.out.println(filename);
+            }
+        }
+    }
 }
